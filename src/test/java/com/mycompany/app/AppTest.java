c55660366155d8cd03ecf7e6c0403ffb9f7f8936
Mahesh0808 <59959646+Mahesh0808@users.noreply.github.com>
2021-02-12 19:07:04 +0530

    two test cases added

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,34 +17,9 @@ public class AppTest
     {
         assertTrue( true );
     }
-    
+  
     @Test
     public void shouldAnswerWithTrue1()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue2()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue3()
-    {
-        assertTrue( true );
-    }
-    
-    
-    @Test
-    public void shouldAnswerWithTrue4()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue5()
     {
         assertTrue( true );
     }
